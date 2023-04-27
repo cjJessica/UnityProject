ffc2c72b7347b9dd513304e2075ddee44f97077c
Charli Pape <cpape125@district.evit.com>
2023-04-27 14:31:37 -0700

    Added slime with animations that aren't applied yet as well as movement

---
+++ Assets/slimeMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slimeMovement : MonoBehaviour
+{
+    Rigidbody2D rbSlime;
+
+    public GameObject SlimeCharacter;
+    public float slimeSpeed = .01f;
+    public Vector2 movement;
+
+    bool slimeFacingRight = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbSlime = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = 1f;
+        transform.Translate(new Vector2(slimeSpeed, 0));
+
+        if (!slimeFacingRight) 
+        {
+            transform.Translate(new Vector2(-slimeSpeed, 0));
+        }
+    }
+
+    private void OnColliderEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Objects")
+        {
+            print("collided");
+            FlipSlime();
+        }
+    }
+
+    void FlipSlime()
+    {
+        slimeFacingRight = !slimeFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+    }
+    
+}
